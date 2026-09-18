548075d8ed1bd5e6b256252fa07fe32868e5351a
Kennedy <nwaforkennedychiedozie@gmail.com>
2026-09-18 15:25:37 -0700

    Fix script source in about.html and enhance login logic in index2.js

--- index2.js
+++ index2.js
@@ -1,4 +1,22 @@
+
 //else if login
 let userName="anderson@gmail.com";
 let userPassword="123456";
 let correctPassword="123456";
+let correctUserName="anderson@gmail.com";
+let hasLogin=false;
+
+//using if condition
+if(userName==correctUserName && userPassword==correctPassword){
+    console.log("Welcome to our site,login successsfull");
+    hasLogin=true;
+}else if(userName !=correctUserName){
+    console.log('your username is incorrect');
+}else if(userPassword != correctPassword){
+    console.log('incorrect password');
+}else{
+    console.log('incorrect login details');
+}
+if(hasLogin){
+    console.log('you can now access our site');
+}
