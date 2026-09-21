74c6a6921867e46125bc89009a3527745592bc3d
Kennedy <nwaforkennedychiedozie@gmail.com>
2026-09-21 02:24:32 -0700

    Refactor ATM app logic and enhance login functionality in index2.js

--- index2.js
+++ index2.js
@@ -20,3 +20,23 @@ if(userName==correctUserName && userPassword==correctPassword){
 if(hasLogin){
     console.log('you can now access our site');
 }
+
+//ATM APP
+let accountName='Kennedy';
+let correctPin=1234;
+let accountBalance=3000;
+let enterPin=1234;
+let moneyWithdrawl=3000;
+
+
+
+if(correctPin===enterPin){
+    console.log(`login successful`);
+    if(accountBalance > moneyWithdrawl){
+   let  currentBalance=accountBalance-moneyWithdrawl;
+   console.log(`current balance: ${currentBalance}`);
+    }
+
+}else{
+    console.log('incorrect password')
+}
